267f1feb4bfd8e2aaafcf90af8fcef750ac36492
prem45764-maker <prem45764@gmail.com>
2026-04-06 13:35:35 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,213 @@
+print("hello world")
+# a = 5
+# b = 4
+# print("sum is:", a + b)
+# a = 13
+# b = 4
+# print("remainder is:", a % b)
+# a = int(input("enter a first number:"))
+# b = int(input("enter a second number:"))
+# greater = a > b
+# print("a is a greater than b:", greater)
+# a = int(input("Enter a first number:"))
+# b = int(input("enter a second number:"))
+# avg = (a + b) /2
+# print("Average:", avg)
+
+# num1 = int(input("Enter a number:"))
+# Squ = num1**2
+# print("calculate the sqaure of a number:", Squ)
+# a = "u83jkshqu"
+# print(a[1:6:4])
+
+# name = "suraj kumar"
+# print(len(name))
+# print(name.endswith("arf"))
+# print(name.startswith("aur"))
+# print(name.capitalize())
+
+# name = str(input("Enter a name:"))
+# print(f" Good afternoon {name}")
+# name = "harry is bad boy  and very kind"
+# print(name.replace("  " , " "))
+# print(name.find("  "))
+
+# letter = "Dear Harry ,\n \tThis python course is nice.\nthanks!"
+# print(letter)
+
+# list = ["suraj" , "rohan" , "suman" , "aman" , "saif" , 15 , "apple"]
+# print(list[1 : 8])
+# print(list[1])
+# list.append("kumar")
+# print(list)
+
+num = [ 131,13,123, 34 ,14  ,35]
+# num.sort() # kam sa zada order ma print kar dtaa ha 
+# num.reverse()# ishma list ko ultan karka print kar data ha 
+# num.insert(3 , 224)# ishma ham kisi bhi index ma ek new value ko print kar data ha 
+# print(num.pop(1))# ishma ham index ki use karka number ko remove kara data ha 
+# num.remove(34)# lekin ishma sidha vaha number lekha kar sidha remove kara skata ah 
+# print(num)
+
+# name = ( "suraj" , "saif" , "aman" , "alok" ,43,43434,23434,324,35,3,6,3,53,2,4,3,3 )
+# print(name.index("alok"))# ishma hamana zo value di ha uska index print hokar ayaga ki uski value kaha ha 
+# print(name.count(3))# ishma hamar zo bhi value di ha uska count gina kar aa jayaga
+# # print(type(name))
+
+# fruits = []
+# f1 = input("enter a fruits name:")
+# fruits.append(f1)
+# f2 = input("enter a fruits name:")
+# fruits.append(f2)
+# f3 = input("enter a fruits name:")
+# fruits.append(f3)
+# f4 = input("enter a fruits name:")
+# fruits.append(f4)
+# f5 = input("enter a fruits name:")
+# fruits.append(f5)
+# f6 = input("enter a fruits name:")
+# fruits.append(f6)
+# f7 = input("enter a fruits name:")
+# fruits.append(f7)
+# print(fruits)
+
+# student_marks = []
+# st1 = int(input("enter a student marks"))
+# student_marks.append(st1)
+# st2 = int(input("enter a student marks"))
+# student_marks.append(st2)
+# st3 = int(input("enter a student marks"))
+# student_marks.append(st3)
+# st4 = int(input("enter a student marks"))
+# student_marks.append(st4)
+# st5 = int(input("enter a student marks"))
+# student_marks.append(st5)
+# st6 = int(input("enter a student marks"))
+# student_marks.append(st6)
+
+# student_marks.sort()
+# print(student_marks)
+# tup = ["suraj" , 23, 34, "alok"]
+# tup[0] = "joke"
+# list = [45 , 67 ,56 ,76]
+# print(sum(list))
+
+# num = (7,0,8,0,0,9)
+# print(num.count(0))
+
+# marks = {
+#     "suraj": 98,
+#     "alok": 78,
+#     "anjali":70,
+# }
+
+# # print(marks , type(marks) , len(marks))
+# # print(marks.items())# ishma hamara marks ek tuple ka form ma print hoga
+# # print(marks.keys())# ishma hama dict ki kry milagi matlab variable jima hamne value store ki ha 
+# # print(marks.values())# agar ishma hamri value print ho jati ha 
+# marks.update({"suraj" : 98 , "rani": 100})# ishma ham apni value change kar data ha ya ham ishma ek new value bhi print kara skaata aha 
+# print(marks.get("alok1"))# ishma hamari alok ka value marks print ho jayga
+# print(marks["alok1"])# dono function ek hi tara kam kar raha ha par lekin ham agar marks get lekhta ha jaise alok 2 to vaha none print karaaga lekin jab ham marks aur alok2 print karta ha to vaha error dataaha yahi bahut bada difference ha
+#set kabhi repeated num ko print nahi karta 
+# s = set()empty set aisha banataa ha 
+# s = {2 , 4 , 5 , 6 , 3}
+# s.add(567)# ishma ham set ma kuc bhi add kara skata aha 
+# s.remove(567)
+# s.pop()# ishma ya koi bhi random number ko set sa remove kar data ha 
+# s.clear()# ishma ham set ko pura clean kar skata ha matlab sara value delete kar skataa ha 
+# print(s, type(s))
+
+# num = {2, 4 , 2 , 8 , 9 , 3}
+# num2 = {2 , 3 , 4 ,113,5,5245,45,45}
+# print(num.union(num2))# ishma hamar do variable num aur num2 ki value add hokar pint ho jat ha 
+# print(num.intersection(num2))# ishma hamar dono set ka value ma zo bhi comman hoga vaha print ho jayga
+
+# words = {
+#     "adami": "men",
+#     "mota" : "fat",
+#     "kala" : "black",
+#     "jao" : "go",
+#     "aao" : "come"
+# }
+
+# w = input("enter a translate word in english")
+# print(words[w])
+
+# num = set()
+# num1 = int(input("enter a first number:"))
+# num.add(num1)
+# num2 = int(input("enter a second number:"))
+# num.add(num2)
+# num3 = int(input("enter a third number:"))
+# num.add(num3)
+# num4 = int(input("enter a fourth number:"))
+# num.add(num4)
+# num5 = int(input("enter a fifth number:"))
+# num.add(num5)
+# num6 = int(input("enter a sixth number:"))
+# num.add(num6)
+# num7 = int(input("enter a seventh number:"))
+# num.add(num7)
+# num8 = int(input("enter a eighth number:"))
+# num.add(num8)
+# print(num)
+
+# num = set()
+# inte = int(input("enter a number:"))
+# stri = str(input("enter a string"))
+# integer = num.add(inte)
+# print(integer)
+# string = num.add(stri)
+# print(string)
+
+# num = [10, 20, 30, 40, 50]
+# print(sum(num))
+# num = (5, 10, 15, 10, 20)
+# print(num.count(10))
+
+# num = set()
+# num1 = int(input("Enter a number"))
+# num.add(num1)
+# num2 = int(input("Enter a number"))
+# num.add(num2)
+# num3 = int(input("Enter a number"))
+# num.add(num3)
+# num4 = int(input("Enter a number"))
+# num.add(num4)
+# num5 = int(input("Enter a number"))
+# num.add(num5)
+# print(num)
+# numbers = [1, 2, 3, 4, 5]
+# numbers.reverse()
+# print(numbers)
+
+marks = []
+marks1 = int(input("Enter a numbers"))
+marks.append(marks1)
+marks2 = int(input("Enter a numbers"))
+marks.append(marks2)
+marks3 = int(input("Enter a numbers"))
+marks.append(marks3)
+marks4 = int(input("Enter a numbers"))
+marks.append(marks4)
+marks5 = int(input("Enter a numbers"))
+marks.append(marks5)
+marks6 = int(input("Enter a numbers"))
+marks.append(marks6)
+total = sum(marks)
+print("total marks:", total)
+perc = (total/600)*100
+print("percent:", perc)
+avg = sum(marks)/6
+print("average:", avg)
+marks.sort()
+print("lowest:", marks[0])
+print("highest:" , marks[-1])
+if avg >= 90:
+    print("Grade A")
+elif avg >= 70:
+    print("Grade B")
+elif avg >= 60:
+    print("Grade C")
+else:
+    print("Fail")
