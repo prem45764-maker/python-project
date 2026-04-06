a33d8e75e6bac0adb1ec588ad0806adda3f825b6
prem45764-maker <prem45764@gmail.com>
2026-04-06 13:31:57 +0530

    Add files via upload

---
+++ hellomaster.py
@@ -0,0 +1,98 @@
+# print("hello world")
+
+# num = int(input("enter a number"))
+# if num % 2 == 0:
+#     print("EVEN")
+# else:
+#     print("ODD")
+# num = int(input("Enter a number: "))
+
+# if num <= 1:
+#     print("Neither prime nor composite")
+# else:
+#     count = 0
+#     for i in range(1, num + 1):
+#         if num % i == 0:
+#             count += 1
+
+#     if count == 2:
+#         print("Prime number")
+#     else:
+#         print("Composite number")
+# num1 = int(input("Enter a first number:"))
+# num2 = int(input("Enter a second number:"))
+# op = input("enter a operator( + , - , * , / ) :")
+# if op == "+":
+#     print(num1 + num2)
+# elif op == "-":
+#     print(num1 - num2)
+# elif op == "*":
+#     print(num1 * num2)
+# elif op == "/":
+#     if num2 != 0:
+#         print(num1 / num2)
+# else:
+#     ("valid the code")
+
+# num = int(input("Enter a number:"))
+# if num % 2 == 0:
+#     print("Even number")
+# else:
+#     print("Odd number")
+
+# num = int(input("Entee a number:"))
+# square = num **2
+# cube = num**3
+# print("square numbers:" ,square)
+# print("cube number:" , cube)
+# num = int(input("Enter a numbers:"))
+# count = 0
+# for i in range(1 , num + 1):
+#     if num % i == 0:
+#         count += 1
+        
+#     if count == 2:
+#         print(i)
+# for num in range(1, 51):
+#     count = 0
+#     for i in range(1 , num + 1):
+#         if num % 1 == 0:
+#             count += 1
+# if count == 2:
+#     print(num, end = " ")
+    
+# 1 se 50 tak ke prime numbers
+
+# for num in range(2, 51):  # 2 se 50 tak ke numbers
+#     count = 0
+#     for i in range(1, num + 1):
+#         if num % i == 0:
+#             count += 1
+#     if count == 2:  # agar exactly 2 divisor hain → prime
+#         print(num, end=" ")
+
+print(''' Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+
+Then the traveler in the dark
+Thanks you for your tiny spark,
+How could he see where to go,
+If you did not twinkle so?
+
+In the dark blue sky you keep,
+Often through my curtains peep
+For you never shut your eye,
+Till the sun is in the sky.
+
+As your bright and tiny spark
+Lights the traveler in the dark,
+Though I know not what you are,
+Twinkle, twinkle, little star.
+''')
